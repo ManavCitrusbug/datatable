e1acf5f791886275a039909294edb378a79fd87a
unknown <manav.citrusbug@gmail.com>
2022-07-01 18:34:42 +0530

    done

--- pr2/db_in_js/static/js/db.js
+++ pr2/db_in_js/static/js/db.js
@@ -1,28 +1,58 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+    // $.ajax({
+    //     url: '/datas/',
+    //     type: 'GET',
+
+    //     success: function (data) {
+    //         var val;
+    //         var len;
+    //         for (var i = 0; i < data.data.length; i++) {
+    //             val = data.data[i];
+
+    //             $('#data').append(`<tr  id= "${val.id}">
+    //             <th  class='text-center'>${val.id}</th>
+    //             <td  class='text-center'>${val.Age}</td>
+    //             <td  class='text-center'>${val.Sex}</td>
+    //             <td  class='text-center'>${val.Job}</td>
+    //             <td  class='text-center'>${val.Housing}</td>
+    //             <td  class='text-center'>${val.Saving_accounts}</td>
+    //             <td  class='text-center'>${val.Checking_account}</td>
+    //             <td  class='text-center'>${val.Credit_amount}</td>
+    //             <td  class='text-center'>${val.Duration}</td>
+    //             <td  class='text-center'>${val.Purpose}</td>
+    //             </tr>
+
+    //             `);
+    //         }
+
+    //     }
+
+    // });
+
+})
+function range_page() {
+    var range = $('#range').val();
     $.ajax({
-        url:'/datas/',
-        type:'GET',
+        url: '/datas/',
+        type: 'GET',
+        success: function (data) {
+            $('#page').html("");
+            $('#data').html("");
+            $('#page').append(`<li class="page-item"><a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>`);
+            var page = data.data.length / range;
+            for (var i = 1; i < page; i++) {
+                $('#page').append("<li class='page-item' id=" + i + ">" + "<a class='page-link' href=" + i + ">" + i + "</a></li>");
 
-        success:function(data){
-
-            for (var i=0;i<data.data.length;i++) {
-                val = data.data[i];
-                $('#data').append("<tr class=' table-active' >");
-                $('#data').append("<td  class='text-center'>"+val.id+"</td>");
-                $('#data').append("<td class='text-center'>"+val.Age+"</td>");
-                $('#data').append("<td class='text-center'>"+val.Sex+"</td>");
-                $('#data').append("<td class='text-center'>"+val.Job+"</td>");
-                $('#data').append("<td class='text-center'>"+val.Housing+"</td>");
-                $('#data').append("<td class='text-center'>"+val.Saving_accounts+"</td>");
-                $('#data').append("<td class='text-center'>"+val.Checking_account+"</td>");
-                $('#data').append("<td class='text-center'>"+val.Credit_amount+"</td>");
-                $('#data').append("<td class='text-center'>"+val.Duration+"</td>");
-                $('#data').append("<td class='text-center'>"+val.Purpose+"</td>");
-                $('#data').append("</tr>");
-                
             }
+            $('#page').append(`<li class="page-item"><a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>`);
+          
+            $('#page').html();
+
         }
 
-    });
-})
+    })
+
+
+
+}
